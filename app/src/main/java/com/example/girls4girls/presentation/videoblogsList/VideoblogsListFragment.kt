82749f0b6574332aa852667ebe8add0d6a6b48ff
SultanSarygulov <itsme.kaan.sarygulov@gmail.com>
2023-02-09 02:38:26 +0600

    Implemented popup category menu

--- app/src/main/java/com/example/girls4girls/presentation/videoblogsList/VideoblogsListFragment.kt
+++ app/src/main/java/com/example/girls4girls/presentation/videoblogsList/VideoblogsListFragment.kt
@@ -3,10 +3,12 @@ package com.example.girls4girls.presentation.videoblogsList
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.util.Log
+import android.view.ContextMenu
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
 import androidx.navigation.fragment.findNavController
 import com.example.girls4girls.R
 import androidx.appcompat.widget.SearchView
@@ -35,6 +37,10 @@ class VideoblogsListFragment : Fragment(), SearchView.OnQueryTextListener {
         setAdapter()
         setSearchView()
 
+        binding.categoryButton.setOnClickListener { view ->
+            showPopUpMenu(view)
+        }
+
         val videos = mutableListOf<VideoBlog>()
 
         for (i in 1..5){
@@ -43,6 +49,41 @@ class VideoblogsListFragment : Fragment(), SearchView.OnQueryTextListener {
         }
         videoAdapter.modifyList(videos)
     }
+
+    private fun showPopUpMenu(view: View) {
+        val popupMenu = PopupMenu(requireContext(), view)
+
+        popupMenu.inflate(R.menu.category_popup_menu)
+
+        popupMenu.setOnMenuItemClickListener { item ->
+            when(item.itemId){
+                R.id.category1 -> {videoAdapter.filter("1")
+                    binding.categoryTxt.text = item.title
+                    true
+                }
+                R.id.category2 -> {videoAdapter.filter("2")
+                    binding.categoryTxt.text = item.title
+                    true
+                }
+                R.id.category3 -> {videoAdapter.filter("3")
+                    binding.categoryTxt.text = item.title
+                    true
+                }
+                R.id.category4 -> {videoAdapter.filter("4")
+                    binding.categoryTxt.text = item.title
+                    true
+                }
+                R.id.category5 -> {videoAdapter.filter("5")
+                    binding.categoryTxt.text = item.title
+                    true
+                }
+                else -> false
+            }
+        }
+
+        popupMenu.show()
+    }
+
 
     private fun setSearchView() {
         binding.searchView.setOnQueryTextListener(this)
