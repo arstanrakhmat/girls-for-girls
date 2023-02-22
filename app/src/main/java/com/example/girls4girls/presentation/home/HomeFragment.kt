fdc1657706261c02f73b1349c5315ea2911d7070
SultanSarygulov <itsme.kaan.sarygulov@gmail.com>
2023-02-22 13:34:05 +0600

    Implemented ViewPager2

--- app/src/main/java/com/example/girls4girls/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/example/girls4girls/presentation/home/HomeFragment.kt
@@ -1,13 +1,11 @@
 package com.example.girls4girls.presentation.home
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.example.girls4girls.R
 import com.example.girls4girls.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -27,14 +25,20 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setImagePager()
+
+        setEventRecycler()
+
+    }
+
+    private fun setImagePager() {
+        binding.mainTeam.adapter = ImagePagerAdapter()
+        binding.mainTeamDotsIndicator.attachTo(binding.mainTeam)
+    }
+
+    private fun setEventRecycler() {
         val eventAdapter = EventAdapter()
-
         binding.eventRecyclerView.adapter = eventAdapter
-
         eventAdapter.submitList(viewModel.list)
-
-
-
-
     }
 }
