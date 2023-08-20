24128c022ec520fed71de69fccc4c9218dfeb565
Arstanbek Rakhmatulaev <rakhmatulaev_a@auca.kg>
2023-08-20 11:01:50 +0300

    Add new url server

--- app/src/main/java/com/example/girls4girls/utils/Constants.kt
+++ app/src/main/java/com/example/girls4girls/utils/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.girls4girls.utils
 
 object Constants {
-    const val BASE_URL = "https://girls4girls.herokuapp.com/api/"
+    const val BASE_URL = "https://girlsforgirls.kg/api/docs"
+//    const val BASE_URL = "https://girls4girls.herokuapp.com/api/"
     const val TOKEN = ""
 }
