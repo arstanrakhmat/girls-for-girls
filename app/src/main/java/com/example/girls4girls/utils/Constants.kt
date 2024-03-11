38a2a7fe7810aaedbf02fab994973e815b6bf056
arstanrakhmat <arstanbek.rakh@gmail.com>
2024-03-11 12:45:52 +0600

    Update BASE_URL

--- app/src/main/java/com/example/girls4girls/utils/Constants.kt
+++ app/src/main/java/com/example/girls4girls/utils/Constants.kt
@@ -1,7 +1,8 @@
 package com.example.girls4girls.utils
 
 object Constants {
-    const val BASE_URL = "https://girlsforgirls.kg/api/docs/"
+//    const val BASE_URL = "https://girlsforgirls.kg/api/docs/"
+    const val BASE_URL = "https://girsl4girls-63d64418cf09.herokuapp.com/api/docs"
 //    const val BASE_URL = "https://girls4girls.herokuapp.com/api/"
     const val TOKEN = ""
 }
